87f7e4b0f3bbf19ed6ff452bf26c3bb70419ad76
Avishai Lippner <avishai.lippner@wework.com>
2017-08-24 14:46:28 +0300

    Add the association between review and restaurant in review_controller mainly. Add limitation in review's view in rating (make it in range of 0 to 3)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,63 +1,110 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
 
-  # GET /reviews
-  # GET /reviews.json
+  # GET /restaurants/:restaurant_id/reviews
+  # GET /restaurants/:restaurant_id/reviews.xml
   def index
-    @reviews = Review.all
+    #1st you retrieve the restaurant thanks to params[:restaurant_id]
+    restaurant = Restaurant.find(params[:restaurant_id])
+    #2nd you get all the reviews of this restaurant
+    @reviews = restaurant.reviews
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => reviews }
+    end
   end
 
-  # GET /reviews/1
-  # GET /reviews/1.json
+  # GET /restaurants/:restaurant_id/reviews/:id
+  # GET /reviews/:id.xml
   def show
+    #1st you retrieve the restaurant thanks to params[:restaurant_id]
+    restaurant = Restaurant.find(params[:restaurant_id])
+    #2nd you retrieve the review thanks to params[:id]
+    @review = restaurant.reviews.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => reviews }
+    end
   end
 
-  # GET /reviews/new
+  # GET /restaurants/:restaurant_id/reviews/new
+  # GET /restaurants/:restaurant_id/reviews/new.xml
   def new
-    @review = Review.new
+    #1st you retrieve the restaurant thanks to params[:restaurant_id]
+    restaurant = Restaurant.find(params[:restaurant_id])
+    #2nd you build a new one
+    @review = restaurant.reviews.build
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => reviews }
+    end
   end
 
-  # GET /reviews/1/edit
+  # GET /restaurants/:restaurant_id/reviews/:id/edit
   def edit
+    #1st you retrieve the restaurant thanks to params[:restaurant_id]
+    restaurant = Restaurant.find(params[:restaurant_id])
+    #2nd you retrieve the review thanks to params[:id]
+    @review = restaurant.reviews.find(params[:id])
   end
 
-  # POST /reviews
-  # POST /reviews.json
+  # POST /restaurants/:restaurant_id/reviews
+  # POST /restaurants/:restaurant_id/reviews.xml
   def create
-    @review = Review.new(review_params)
+    #1st you retrieve the restaurant thanks to params[:restaurant_id]
+    restaurant = Restaurant.find(params[:restaurant_id])
+    #2nd you create the review with arguments in params[:reviews]
+    @review = restaurant.reviews.create!(review_params)
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+        #1st argument of redirect_to is an array, in order to build the correct route to the nested resource review
+        format.html { redirect_to([@review.restaurant, @review], notice: 'Review was successfully created.') }
+        #the key :location is associated to an array in order to build the correct route to the nested resource review
+        format.xml  { render :xml => @review, status: :created, location: [@review.restaurant, @review] }
       else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @review.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
+  # PUT /restaurants/:restaurant_id/reviews/:id
+  # PUT /restaurants/:restaurant_id/reviews/:id.xml
   def update
+    #1st you retrieve the restaurant thanks to params[:restaurant_id]
+    restaurant = Restaurant.find(params[:restaurant_id])
+    #2nd you retrieve the review thanks to params[:id]
+    @review = restaurant.reviews.find(params[:id])
+
     respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
+      if @review.update_attributes(review_params)
+        #1st argument of redirect_to is an array, in order to build the correct route to the nested resource review
+        format.html { redirect_to([@review.restaurant, @review], :notice => 'Review was successfully updated.') }
+        format.xml  { head :ok }
       else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @review.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
+  # DELETE /restaurants/:restaurant_id/reviews/1
+  # DELETE /restaurants/:restaurant_id/reviews/1.xml
   def destroy
+    #1st you retrieve the restaurant thanks to params[:restaurant_id]
+    restaurant = Restaurant.find(params[:restaurant_id])
+    #2nd you retrieve the review thanks to params[:id]
+    @review = restaurant.reviews.find(params[:id])
     @review.destroy
+
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
-      format.json { head :no_content }
+      #1st argument reference the path /restaurants/:restaurant_id/reviews/
+      format.html { redirect_to(restaurant_reviews_url) }
+      format.xml  { head :ok }
     end
   end
 
