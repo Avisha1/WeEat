673909e8c7b5a2bc37e3ede353e457d7d02582a4
Avishai Lippner <avishai.lippner@wework.com>
2017-08-31 18:29:36 +0300

    Start basic form of form :) with new restaurant

--- app/javascript/packs/NewRestaurant.jsx
+++ app/javascript/packs/NewRestaurant.jsx
@@ -32,7 +32,14 @@ export default class NewRestaurant extends React.Component {
     //     });
     // };
 
+    cuisines = ['Indian','French', 'Italian', 'Cafe', 'Bakery', 'fish', 'Pizza', 'Burger'];
+
+
     newRestaurantSubmit() {
+
+        //valid params
+
+        //make post call
 
     }
 
@@ -41,21 +48,48 @@ export default class NewRestaurant extends React.Component {
 
             <div>
                 {console.log('NAVIGATION', 'NewRestaurant page')}
+                <br />
+                <h3> Add New Restaurant: </h3>
+                <br />
                 <form className="restaurant-form" onSubmit={this.newRestaurantSubmit}>
-                    {/*<h1>New Restaurant</h1>*/}
-                    {/*<form className="restaurant-form" onSubmit={this.newRestaurantSubmit}>*/}
-                    {/*<div className="form-inputs"/>*/}
-                    {/*{this.renderNameInput()}*/}
-                    {/*{this.renderCuisineSelect()}*/}
-                    {/*{this.renderTenbisCheck()}*/}
-                    {/*{this.renderAddressInput()}*/}
-                    {/*{this.renderMaxDeliveryTimeSelect()}*/}
-                    {/*<div className='row'>*/}
-                    {/*<div className='col-sm-4'>*/}
-                    {/*<input type="submit" value="Save" className='btn btn-primary' />*/}
-                    {/*</div>*/}
-                    {/*</div>*/}
-                    {/*</form>*/}
+
+                    <div className="row">
+
+                        <div className="col-xs-4 col-sm-5">
+                            <label>Restaurant name:</label>
+                            <input type="text" className="form-control" placeholder="Write here the restaurant's name"/>
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div className="row">
+                        <div className="col-xs-4 col-sm-5">
+                            <label>Address:</label>
+                            <input type="text" className="form-control" placeholder="Address"/>
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div className="row">
+                        <div className="col-xs-4 col-sm-5">
+                            <label>Accept 10 bis? <input className="form-check-input" type="checkbox" value=""/></label>
+                        </div>
+                    </div>
+
+                    <br />
+
+
+                    <label>Cuisines:</label>
+                    <br />
+                    <select className="custom-select col-xs-4 col-sm-5" id="inlineFormCustomSelectPref">
+                        <option value="" defaultValue>Choose...</option>
+                        {this.cuisines.map((item, key) => {
+                            return (<option key={key} >{item}</option>)
+                        })}
+                    </select>
+                    
                 </form>
 
             </div>
